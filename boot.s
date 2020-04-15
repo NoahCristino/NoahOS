ba62a4a4031a1300c20eafd54ab7c94167070f9c
Noah Cristino <noahcristino@yahoo.com>
2020-04-15 15:39:43 -0400

    cleaned up boot.o

--- boot.s
+++ boot.s
@@ -1,29 +1,18 @@
-# set flags to 0
 .set FLAGS,    0
-
-# set magic number to 0x1BADB002 to identified by bootloader 
 .set MAGIC,    0x1BADB002
-
-# set the checksum
 .set CHECKSUM, -(MAGIC + FLAGS)
 
-# set multiboot enabled
 .section .multiboot
 
-# define type to long for each data defined as above
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
 
-
-# set the stack bottom 
 stackBottom:
 
-# define the maximum size of stack to 512 uint8_ts
 .skip 4096
 
 
-# set the stack top which grows from higher to lower
 stackTop:
 
 .section .text
@@ -32,18 +21,11 @@ stackTop:
 
 
 _start:
-
-
-  # assign current stack pointer location to stackTop
 	mov $stackTop, %esp
-
-  # call the kernel main function
 	call kernel_entry
-
 	cli
 
 
-# put system in infinite loop
 hltLoop:
 
 	hlt
